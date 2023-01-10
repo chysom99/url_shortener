78ad5cc47c283544b4db6b6a7aae09d8c0e96fb9
chisom <chisom.ofoedu@flutterwavego.com>
2023-01-10 14:49:59 +0100

    token authentication

---
+++ src/api/middleware/auth_token.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = async (req, res, next) => {
+  try {
+    const auth = req.headers.authorization;
+
+    if (auth) {
+      const splitValue = auth.split(" ");
+      const token = splitValue[1];
+
+      if (token) {
+        const result = jwt.verify(token, process.env.secretKey);
+
+        if (result) {
+          req.user_id = result.id;
+          next();
+        } else {
+          return res.status(401).json({ messages: "inavlid token" });
+        }
+      } else {
+        return res.status(401).json({ messages: "inavlid token" });
+      }
+    } else {
+      return res.status(401).json({ messages: "inavlid token" });
+    }
+  } catch (err) {
+    return res.status(401).json({ messages: "inavlid token" });
+  }
+};
